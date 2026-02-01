c138d269957bd1cf63eb2afb1c0ce18032336542
Ian Spence <50440569+IanMacSpence@users.noreply.github.com>
2026-02-01 16:34:28 +0200

    bug fix(business card): fixed small file errors

--- curriculum/15-react-js-fundamentals/15-2-solo-project-business-card/scrimba-business-card-section-15-react-fundamentals/src/components/SocialIcon.jsx
+++ curriculum/15-react-js-fundamentals/15-2-solo-project-business-card/scrimba-business-card-section-15-react-fundamentals/src/components/SocialIcon.jsx
@@ -1,9 +1,10 @@
-export default function SocialIcon({icon: Icon, href, ...props}){
+export default function SocialIcon({icon: Icon, href}){
     // need to include react-icon as a react component in the return, so need to put it in a variable first to do this
     return(
         <a className="icon-box" 
         href={href} 
-        target="_blank">
+        target="_blank"
+        rel="noopener noreferrer">
             <Icon />
         </a>
     )
