6985e03913a58c43291795ff6a6b2ccb0cae5023
Ian Spence <50440569+IanMacSpence@users.noreply.github.com>
2026-02-14 22:53:58 +0200

    Solo Project(Quizzical App): Initial commit

---
+++ curriculum/15-react-js-fundamentals/15-8-solo-project-quiz-app/scrimba-quiz-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Quizzical</h1>
+      <p>Some description if needed</p>
+      <button>Start quiz</button>
+    </>
+  )
+}
+
+export default App
