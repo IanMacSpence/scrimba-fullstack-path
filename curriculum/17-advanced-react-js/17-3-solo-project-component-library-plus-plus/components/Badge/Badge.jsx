9436fedfa1353773ec3dff7db4334574779dd165
Ian Spence <50440569+IanMacSpence@users.noreply.github.com>
2026-03-28 13:22:00 +0200

    Add components and structure and style for Badge
    - Files were added for all components and their styles.
    
    - The jsx for Badge was added as well as styles.

---
+++ curriculum/17-advanced-react-js/17-3-solo-project-component-library-plus-plus/components/Badge/Badge.jsx
@@ -0,0 +1,12 @@
+import clsx from 'clsx';
+import "./Badge.css"
+
+export default function Badge({children, className, variant, color, ...rest}){
+    let variantClass = variant && `badge--${variant}`
+    let colorClass = color && `badge--${color}`
+    const allClasses = clsx("badge", variantClass, colorClass, className)
+
+    return(
+        <span className={allClasses}>{children}</span>
+    )
+}
--- curriculum/17-advanced-react-js/17-3-solo-project-component-library-plus-plus/index.jsx
+++ curriculum/17-advanced-react-js/17-3-solo-project-component-library-plus-plus/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import Menu from "./components/Menu/index";
 import Star from "./components/Star";
 import Button from "./components/Button/Button";
+import Badge from "./components/Badge/Badge";
 
 function App() {
   return (
@@ -41,6 +42,70 @@ function App() {
       <hr />
       <h2 className="section-header">Badges</h2>
       <section className="badge-container"></section>
+      <Badge>Badge</Badge>
+      <Badge color="gray" variant="square">
+        Gray Square
+      </Badge>
+      <Badge color="gray" variant="pill">
+        Gray Pill
+      </Badge>
+
+      {/* Green */}
+      <Badge color="green" variant="square">
+        Green Square
+      </Badge>
+      <Badge color="green" variant="pill">
+        Green Pill
+      </Badge>
+
+      {/* Blue */}
+      <Badge color="blue" variant="square">
+        Blue Square
+      </Badge>
+      <Badge color="blue" variant="pill">
+        Blue Pill
+      </Badge>
+
+      {/* Indigo */}
+      <Badge color="indigo" variant="square">
+        Indigo Square
+      </Badge>
+      <Badge color="indigo" variant="pill">
+        Indigo Pill
+      </Badge>
+
+      {/* Purple */}
+      <Badge color="purple" variant="square">
+        Purple Square
+      </Badge>
+      <Badge color="purple" variant="pill">
+        Purple Pill
+      </Badge>
+
+      {/* Pink */}
+      <Badge color="pink" variant="square">
+        Pink Square
+      </Badge>
+      <Badge color="pink" variant="pill">
+        Pink Pill
+      </Badge>
+
+      {/* Red */}
+      <Badge color="red" variant="square">
+        Red Square
+      </Badge>
+      <Badge color="red" variant="pill">
+        Red Pill
+      </Badge>
+
+      {/* Yellow */}
+      <Badge color="yellow" variant="square">
+        Yellow Square
+      </Badge>
+      <Badge color="yellow" variant="pill">
+        Yellow Pill
+      </Badge>
+
       <hr />
       <h2 className="section-header">Banners</h2>
       <section className="banner-container"></section>
